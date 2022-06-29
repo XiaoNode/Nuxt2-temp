3300784c15f6bfb8ae4116d1478db9be93b0adec
jone.xue <905044086@qq.com>
2022-06-29 15:39:18 +0800

    feat：添加格式化工具

--- env.js
+++ env.js
@@ -1,27 +1,26 @@
 module.exports = {
   // 开发环境
   dev: {
-    NODE_ENV: 'development',
+    NODE_ENV: "development",
     // 统一登录
-    LOGIN_URL: 'http://xxx',
-    SERVER_URL: 'http://localhost:7788',
-    SYSTEM_PORT: '8888'
+    LOGIN_URL: "http://xxx",
+    SERVER_URL: "http://localhost:7788",
+    SYSTEM_PORT: "8888",
   },
   // 测试环境
   test: {
-    NODE_ENV: 'test',
-     // 统一登录
-    LOGIN_URL: 'http://yyy',
-    SERVER_URL: 'https://yyy.yyy',
-    SYSTEM_PORT: '8888'
-
+    NODE_ENV: "test",
+    // 统一登录
+    LOGIN_URL: "http://yyy",
+    SERVER_URL: "https://yyy.yyy",
+    SYSTEM_PORT: "8888",
   },
   // 生产环境
   pro: {
-    NODE_ENV: 'production',
+    NODE_ENV: "production",
     // 统一登录
-    LOGIN_URL: 'http://xxx',
-    SERVER_URL: 'https://xxx.xxx',
-    SYSTEM_PORT: '8888'
-  }
-}
+    LOGIN_URL: "http://xxx",
+    SERVER_URL: "https://xxx.xxx",
+    SYSTEM_PORT: "8888",
+  },
+};
